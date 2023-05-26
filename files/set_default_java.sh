d0e39e53e584cc1ea689828af95bee5d3450160b
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-05-26 08:19:20 +0000

    Improve 'set_default_java.sh' script.

--- files/set_default_java.sh
+++ files/set_default_java.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-JAVA_11=$(alternatives --display java | grep 'family java-11-openjdk' | cut -d' ' -f1)
-#echo $JAVA_11
-alternatives --set java $JAVA_11
+JAVA_DEFAULT=$(alternatives --display java | grep "family java-$1-openjdk" | cut -d' ' -f1)
+#echo $JAVA_DEFAULT
+alternatives --set java $JAVA_DEFAULT
 
 
