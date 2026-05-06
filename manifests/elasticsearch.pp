809b9048503297f40fb80ccb1f9632e75b14f2da
Ivan I. Atanasov <i.atanasov77@gmail.com>
2026-05-06 07:24:01 +0000

    Send Config ElasticSearch Module.

---
+++ manifests/elasticsearch.pp
@@ -0,0 +1,39 @@
+#######################################################################################################
+# Install Plugins
+# ================
+# https://github.com/voxpupuli/puppet-elasticsearch#plugins
+#
+# How to use dejavu for elasticsearch on local running it from opensource.appbase.io
+# ====================================================================================
+# https://stackoverflow.com/questions/50231296/how-to-use-dejavu-for-elasticsearch-on-local-running-it-from-opensource-appbase
+#######################################################################################################
+class vs_core::elasticsearch (
+    Variant[String, Boolean] $version = false,
+    Variant[String, Boolean] $yumRepo = false,
+    
+    Enum['http', 'https'] $apiProtocol = 'https',
+    String $apiHost,
+    Integer[0, 65535] $apiPort,
+    Integer $apiTimeout = 10,
+    String $apiUsername = 'elastic',
+    String $apiPassword = 'elastic',
+    
+    Hash $apiConfig,
+) {
+    class { 'vs_core::elasticsearch::install':
+        version     => $version,
+        yumRepo     => $yumRepo,
+        
+        apiProtocol => $apiProtocol,
+        apiHost     => $apiHost,
+        apiPort     => $apiPort,
+        apiUsername => $apiUsername,
+        apiPassword => $apiPassword,
+        
+        apiConfig   => $apiConfig,
+    }
+    
+    # Error: Could not find a suitable provider for elasticsearch_plugin
+    #######################################################################
+    #class { 'vs_core::elasticsearch::gui': }
+}
---
+++ manifests/elasticsearch/install.pp
@@ -0,0 +1,58 @@
+class vs_core::elasticsearch::install (
+    Variant[String, Boolean] $version,
+    Variant[String, Boolean] $yumRepo = false,
+    
+    Enum['http', 'https'] $apiProtocol = 'https',
+    String $apiHost,
+    Integer[0, 65535] $apiPort,
+    Integer $apiTimeout = 10,
+    String $apiUsername = 'elastic',
+    String $apiPassword = 'elastic',
+    
+    Hash $apiConfig,
+) {
+    if ( $yumRepo == false ) {
+        $manageRepo = true
+        $requiredPackages   = []
+    } else {
+        $manageRepo = false
+        $requiredPackages   = [ Yumrepo['elasticsearch'] ]
+    }
+    
+    if ( $yumRepo != false ) {
+        yumrepo { 'elasticsearch':
+            ensure      => 'present',
+            descr       => "Elasticsearch repository for ${yumRepo} packages",
+            baseurl     => "https://artifacts.elastic.co/packages/${yumRepo}/yum",
+            gpgkey      => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
+            enabled     => true,
+            gpgcheck    => '1',
+            #target      => '/etc/yum.repo.d/puppetlabs.repo',
+        }
+    }
+
+    ###########################################################################################
+    # In Elastic Search 8.x
+    # =======================
+    # Aborting auto configuration because the node keystore contains password settings already
+    ###########################################################################################
+    class { 'elasticsearch':
+        version                 => $version,
+        manage_repo             => $manageRepo,
+        
+        api_protocol            => "${apiProtocol}",
+        api_host                => "${apiHost}",
+        api_port                => $apiPort,
+        api_timeout             => $apiTimeout,
+        api_basic_auth_username => "${apiUsername}",
+        api_basic_auth_password => "${apiPassword}",
+        
+        api_ca_file             => undef,
+        api_ca_path             => undef,
+        validate_tls            => true,
+        
+        config                  => $apiConfig,
+        
+        require                 => $requiredPackages,
+    }
+}
